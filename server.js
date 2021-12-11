7a7def5d8b1c6a96ed9a7302b4f01cf5001427ad
Amanda <amandamsilvasantos96@gmail.com>
2021-12-11 10:50:37 -0300

    configurando a porta

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5005;
 
 
 app.listen(PORT, () => console.log("Servidor rodando na porta " + PORT));
